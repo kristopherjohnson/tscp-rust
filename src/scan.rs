a1fe6f59bee8fb91d696d2b49438fe8ce69e29b3
Kristopher Johnson <kris@kristopherjohnson.net>
2020-12-01 19:58:00 -0500

    Run rustfmt

--- src/scan.rs
+++ src/scan.rs
@@ -91,7 +91,9 @@ pub fn scan_token_from(reader: &mut dyn Read) -> io::Result<String> {
     // convert bytes to a String
     let s = match String::from_utf8(bytes) {
         Ok(s) => s,
-        Err(err) => return Err(io::Error::new(io::ErrorKind::InvalidData, err)),
+        Err(err) => {
+            return Err(io::Error::new(io::ErrorKind::InvalidData, err))
+        }
     };
     Ok(s)
 }

kristopherjohnson/tscp-rust

commit 05ce54c1714c7e96a128d87d74702773708c702e

Use i64 and #[inline] in places. Improves performance on 64-bit macOS

src/eval.rs
@@ -214,6 +214,7 @@ pub fn eval(d: &mut Data) -> Int {
     }
 }
 
+#[inline(always)]
 fn eval_light_pawn(d: &Data, sq: usize) -> Int {
     // the value to return
     let mut r = 0;
@@ -256,6 +257,7 @@ fn eval_light_pawn(d: &Data, sq: usize) -> Int {
     r
 }
 
+#[inline(always)]
 fn eval_dark_pawn(d: &Data, sq: usize) -> Int {
     // the value to return
     let mut r = 0;
@@ -298,6 +300,7 @@ fn eval_dark_pawn(d: &Data, sq: usize) -> Int {
     r
 }
 
+#[inline(always)]
 fn eval_light_king(d: &Data, sq: usize) -> Int {
     // the value to return
     let mut r = KING_PCSQ[sq];
@@ -337,34 +340,31 @@ fn eval_light_king(d: &Data, sq: usize) -> Int {
 
 /// eval_lkp(f) evaluates the Light King Pawn on file f
 
+#[inline(always)]
 fn eval_lkp(d: &Data, f: usize) -> Int {
     let mut r = 0;
 
     let rank_light = d.pawn_rank[LIGHT as usize][f];
 
-    if rank_light == 6 {
-        // pawn hasn't moved
-    } else if rank_light == 5 {
-        r -= 10; // pawn moved one square
-    } else if rank_light != 0 {
-        r -= 20; // pawn moved more than one square
-    } else {
-        r -= 25; // no pawn on this file
+    match rank_light {
+        6 => (),      // pawn hasn't moved
+        5 => r -= 10, // pawn moved one square
+        0 => r -= 25, // no pawn on this file
+        _ => (),      // pawn moved more than one square
     }
 
     let rank_dark = d.pawn_rank[DARK as usize][f];
 
-    if rank_dark == 7 {
-        r -= 15; // no enemy pawn
-    } else if rank_dark == 5 {
-        r -= 10; // enemy pawn on the 3rd rank
-    } else if rank_dark == 4 {
-        r -= 5; // enemy pawn on the 4th rank
+    match rank_dark {
+        7 => r -= 15, // no enemy pawn
+        5 => r -= 10, // enemy pawn on the 3rd rank
+        4 => r -= 5,  // enemy pawn on the 4th rank
+        _ => (),
     }
-
     r
 }
 
+#[inline(always)]
 fn eval_dark_king(d: &Data, sq: usize) -> Int {
     let mut r = KING_PCSQ[FLIP[sq]];
 
@@ -392,29 +392,26 @@ fn eval_dark_king(d: &Data, sq: usize) -> Int {
     r
 }
 
+#[inline(always)]
 fn eval_dkp(d: &Data, f: usize) -> Int {
     let mut r = 0;
 
     let rank_dark = d.pawn_rank[DARK as usize][f];
 
-    if rank_dark == 1 {
-        // do nothing
-    } else if rank_dark == 2 {
-        r -= 10;
-    } else if rank_dark != 7 {
-        r -= 20;
-    } else {
-        r -= 25;
+    match rank_dark {
+        1 => (),
+        2 => r -= 10,
+        7 => r -= 25,
+        _ => r -= 20,
     }
 
     let rank_light = d.pawn_rank[LIGHT as usize][f];
 
-    if rank_light == 0 {
-        r -= 15;
-    } else if rank_light == 2 {
-        r -= 10;
-    } else if rank_light == 3 {
-        r -= 5;
+    match rank_light {
+        0 => r -= 15,
+        2 => r -= 10,
+        3 => r -= 5,
+        _ => (),
     }
 
     r

src/board.rs
@@ -502,9 +502,9 @@ pub fn makemove(d: &mut Data, m: MoveBytes) -> bool {
     d.castle &= CASTLE_MASK[m.from as usize] & CASTLE_MASK[m.to as usize];
     if (m.bits & 8) != 0 {
         if d.side == LIGHT {
-            d.ep = Int::from(m.to) + 8;
+            d.ep = m.to as Int + 8;
         } else {
-            d.ep = Int::from(m.to) - 8;
+            d.ep = m.to as Int - 8;
         }
     } else {
         d.ep = -1;
@@ -518,7 +518,7 @@ pub fn makemove(d: &mut Data, m: MoveBytes) -> bool {
     // move the piece
     d.color[m.to as usize] = d.side;
     if (m.bits & 32) != 0 {
-        d.piece[m.to as usize] = Int::from(m.promote);
+        d.piece[m.to as usize] = m.promote as Int;
     } else {
         d.piece[m.to as usize] = d.piece[m.from as usize];
     }

src/defs.rs
@@ -5,7 +5,7 @@
 //
 // Rust port by Kristopher Johnson
 
-pub type Int = i32;
+pub type Int = i64;
 
 pub const GEN_STACK: usize = 1120;
 pub const MAX_PLY: usize = 32;
@@ -41,12 +41,14 @@ pub const F8: usize = 5;
 pub const G8: usize = 6;
 pub const H8: usize = 7;
 
+/// Get the row number for a square
 macro_rules! row {
     ( $x:expr ) => {
         $x >> 3
     };
 }
 
+/// Get the column index for a square
 macro_rules! col {
     ( $x:expr ) => {
         $x & 7
@@ -88,25 +90,25 @@ pub union Move {
 
 impl Move {
     /// safely extract MoveBytes variant from a Move
-    #[inline]
+    #[inline(always)]
     pub fn bytes(self: Move) -> MoveBytes {
         unsafe { self.b }
     }
 
     /// safely set MoveBytes variant in a Move
-    #[inline]
+    #[inline(always)]
     pub fn set_bytes(self: &mut Move, m: MoveBytes) {
         self.b = m
     }
 
     /// safely extract 32-bit value from a Move
-    #[inline]
+    #[inline(always)]
     pub fn value(self: Move) -> Int {
         unsafe { self.u }
     }
 
     /// safely set 32-bit value for a Move
-    #[inline]
+    #[inline(always)]
     pub fn set_value(self: &mut Move, value: Int) {
         self.u = value;
     }

src/lib.rs
@@ -98,14 +98,13 @@ pub fn parse_move(d: &Data, s: &str) -> Int {
 pub fn move_str(m: MoveBytes) -> String {
     unsafe {
         let from_col =
-            char::from_u32_unchecked(u32::from(col!(m.from)) + 'a' as u32);
+            char::from_u32_unchecked(col!(m.from) as u32 + 'a' as u32);
         let from_row = 8 - row!(m.from);
-        let to_col =
-            char::from_u32_unchecked(u32::from(col!(m.to)) + 'a' as u32);
+        let to_col = char::from_u32_unchecked(col!(m.to) as u32 + 'a' as u32);
         let to_row = 8 - row!(m.to);
 
         if (m.bits & 32) != 0 {
-            let c = match Int::from(m.promote) {
+            let c = match m.promote as Int {
                 KNIGHT => 'n',
                 BISHOP => 'b',
                 ROOK => 'r',
@@ -394,8 +393,8 @@ pub fn bench(d: &mut Data) {
         println!("(invalid)");
         return;
     }
-    let nps = f64::from(d.nodes) / f64::from(t[0]);
-    let nps = nps * 1000.0;
+    let nps = d.nodes / t[0];
+    let nps = nps as f64 * 1000.0;
 
     // Score: 1.00 = my Athlon XP 2000+
     println!(

commit 1721c258b6f17cc23bd31b0d0f63fdd39dc5577d

Fix bug in eval_lkp() introduced in commit 05ce54c while converting an if-else to a match

src/eval.rs
@@ -344,7 +344,7 @@ fn eval_lkp(d: &Data, f: usize) -> Int {
         6 => (),      // pawn hasn't moved
         5 => r -= 10, // pawn moved one square
         0 => r -= 25, // no pawn on this file
-        _ => (),      // pawn moved more than one square
+        _ => r -= 20, // pawn moved more than one square
     }
 
     let rank_dark = d.pawn_rank[IDARK][f];